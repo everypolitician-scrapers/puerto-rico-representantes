f3a5862f0d1f3b5db1def0a53d8fab29a2cce447
Tony Bowden <tony@tmtm.com>
2017-01-03 20:12:05 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -10,7 +11,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -21,20 +22,20 @@ end
 def scrape_list(url)
   noko = noko_for(url)
   noko.css('div.info-block div.info-wrap').each do |person|
-    data = { 
-      id: person.css('a.more-info/@href').text[/rep=(\d+)/, 1],
-      name: person.xpath('.//span[@class="info"]//span[@class="name"]/text()').text.split(' - ').first.tidy.sub('Hon. ', ''),
-      party: person.css('.info .party').text.tidy,
-      area: person.css('.info .district').text.tidy,
-      image: person.css('.identity img/@src').text,
-      phone: person.xpath('.//span[@class="data-type" and contains(.,"Tel:")]').map { |n| n.text.sub('Tel:','').tidy }.join(" / "),
-      fax: person.xpath('.//span[@class="data-type" and contains(.,"Fax:")]').map { |n| n.text.sub('Fax:','').tidy }.join(" / "),
+    data = {
+      id:           person.css('a.more-info/@href').text[/rep=(\d+)/, 1],
+      name:         person.xpath('.//span[@class="info"]//span[@class="name"]/text()').text.split(' - ').first.tidy.sub('Hon. ', ''),
+      party:        person.css('.info .party').text.tidy,
+      area:         person.css('.info .district').text.tidy,
+      image:        person.css('.identity img/@src').text,
+      phone:        person.xpath('.//span[@class="data-type" and contains(.,"Tel:")]').map { |n| n.text.sub('Tel:', '').tidy }.join(' / '),
+      fax:          person.xpath('.//span[@class="data-type" and contains(.,"Fax:")]').map { |n| n.text.sub('Fax:', '').tidy }.join(' / '),
       contact_form: person.css('a.mail/@href').text,
-      term: 29,
-      source: person.css('a.more-info/@href').text,
+      term:         29,
+      source:       person.css('a.more-info/@href').text,
     }
     %i(image contact_form source).each { |field| data[field] = URI.join(url, data[field]).to_s unless data[field].to_s.empty? }
-    ScraperWiki.save_sqlite([:id, :term], data)
+    ScraperWiki.save_sqlite(%i(id term), data)
   end
 end
 
